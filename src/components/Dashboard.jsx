9b5b809b52614f59053ec6b55f7e2625db21dd88
freelancerjob114-oss <freelancerjob114@gmail.com>
2026-04-05 12:18:49 +0530

    Create main trading dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Main Trading Dashboard</h1>
+            {/* Additional components and features will go here */}
+        </div>
+    );
+};
+
+export default Dashboard;
